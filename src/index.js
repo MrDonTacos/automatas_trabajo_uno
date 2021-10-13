196df22cdad47d0e3f5eac480ffe34f3e29b6f7e
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2021-10-13 00:00:33 -0700

    chore: add top leve module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import Player from "/src/player.js";
+
 let canvas = document.getElementById("screen")
 let ctx = canvas.getContext('2d')
 
 const SCREEN_WIDTH = canvas.offsetWidth
 const SCREEN_HEIGHT = canvas.offsetHeight
+debugger
+ctx.clearRect(0,0,SCREEN_WIDTH, SCREEN_HEIGHT);
+
+let goose = new Player(SCREEN_WIDTH, SCREEN_HEIGHT); 
+goose.draw(ctx);
