675d2d5a688560a98652c4838f3225712d22e6f4
Guillermo-CM <Guillermo.castm@hotmail.com>
2021-10-13 00:12:18 -0700

    Crear JS de Malo  y subir assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+import Malo from "../src/malo";
+
+
+// Cosas chuscas del sapo
+
+let imgMalo = document.getElementById('img_sapo')
+
 let canvas = document.getElementById("screen")
 let ctx = canvas.getContext('2d')
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+    ctx.drawImage(imgMalo, 10, 10, 16, 16);
+
+    requestAnimationFrame(gameLoop);
+}
+
+
 
 const SCREEN_WIDTH = canvas.offsetWidth
 const SCREEN_HEIGHT = canvas.offsetHeight
---
+++ src/malo.js
@@ -0,0 +1,5 @@
+export default class Malo {
+    constructor() {
+        this.image = document.getElementById("img_sapo")
+    }
+}
